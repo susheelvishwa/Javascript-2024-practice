0869b1ae4a4643370192f4d685de60e9329f60ca
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-03-08 01:28:42 +0530

    JSON.stugify cover

---
+++ interview prepration/script.js
@@ -0,0 +1,30 @@
+// what is the output of this code
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log([1,2] == [1,2]);
+
+// Q.2
+// const user1 = {
+//   name: "susheel;",
+//   cource: "full stack developer",
+//   institute: "masai school",
+//   address: {
+//     city: "bangaluru",
+//     state: "karnataka",
+//   },
+// };
+// const user2 = JSON.parse(JSON.stringify(user1))
+// const user3 = JSON.parse(JSON.stringify(user1));
+// user2.name = "sonu";
+// user3.name = "pillu";
+// user2.address.city = "raichur";
+// user3.address.city = "pune";
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
+
+// Q.3
+
+// let skills = ["html", "css", "javascript", "react"]; 
+let str = "my name is susheel"
+
